1e81dd79fada6f327abc2c57bd4b9c2f2b668c14
tw1ttt3r <tw1ttt3r@gmail.com>
2026-06-24 00:14:52 -0600

    feat: implemented logic companies

--- app.py
+++ app.py
@@ -1,45 +1,36 @@
 import requests
 from constants import URL
 from bs4 import BeautifulSoup
+import time
 
 print(":::::Iniciando proceso:::::")
 
 response = requests.get(URL)
+allCompanies = []
 
 if response.status_code == 200:
   ## exito
   print("Datos obtenidos:")
   sopa = BeautifulSoup(response.text, 'html.parser')
   lista = sopa.find(class_='operators-list')
-  #print(lista)
   elements = lista.find_all("li")
   for el in elements:
     name = el.get('data-name')
-    els = el.select("div.accordion-body")
+    els = el.select("div.accordion-body > a")
     if len(els) > 0:
-      pass
+      for e in els:
+        url = e.get('href')
+        name = e.text
+        allCompanies.append(dict(name=name, url=url))
     else:
       a = el.find('a')
       url = a.get('href')
-      print(name, url)
-
+      allCompanies.append(dict(name=name, url=url))
 else:
   ## error
   print("Error:", response.status_code)
 
-print(":::::Proceso finalizado:::::")
+data = dict(version=int(time.time()), companies=allCompanies)
+print("data", data)
 
-"""
-<li class="accordion-item" data-list-item-id="e57e084ee04ca84f1d2d349fb27b812b9" data-name="Dalefon">
-  <p>
-    <button aria-expanded="false" class="accordion-header" type="button">
-      <span class="acc-name">Dalefon</span><span class="acc-badge">2</span>
-      <span class="acc-arrow"></span>
-    </button>
-  </p>
-  <div class="accordion-body">
-    <a class="operator-btn" href="https://www.dalefon.mx/vinculatulinea" rel="noopener" target="_blank">Dalefon</a>
-    <a class="operator-btn" href="https://www.internetbienestarmex.com/vinculatulinea" rel="noopener" target="_blank">Dalefon/Internet para el Bienestar</a>
-  </div>
-</li>
-"""
+print(":::::Proceso finalizado:::::")
