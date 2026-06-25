15570b8e0e5addc52cd7e8ba95057774e1cafcdb
tw1ttt3r <tw1ttt3r@gmail.com>
2026-06-24 23:39:28 -0600

    feat: added new constants

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
-from typing import Final
+from typing import Final, LiteralString
 
 # Valores constantes
 URL: Final = "https://portal.crt.gob.mx/plataformas-de-consulta-de-las-companias-telefonicas"
+fileJson: Final = "companies.json"
